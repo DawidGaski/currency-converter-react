005d890ed91a9c5889d39f01bf8403a5f62d221e
Dawid Galeski <dawidgaski@gmail.com>
2023-10-16 12:41:20 +0200

    created currencies

---
+++ src/currencies.js
@@ -0,0 +1,19 @@
+const currencies = [
+    {
+        description: 'Yen',
+        name: 'JPY',
+        rate: 0.034,
+    },
+    {
+        description: 'Dolar Amerykański',
+        name: 'USD',
+        rate: 4.33,
+    },
+    {
+        description: 'Euro',
+        name: 'EUR',
+        rate: 4.66,
+    },
+];
+
+export default currencies;
