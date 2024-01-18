5c0068dd5150bb9b9063e28f24a5533f662bf03e
Dawid Galeski <dawidgaski@gmail.com>
2024-01-18 18:30:22 +0100

    added ADA const [currency, setCurrency] = useState("ADA");

--- src/Form/index.js
+++ src/Form/index.js
@@ -28,7 +28,7 @@ const Form = () => {
     });
   };
 
-  const [currency, setCurrency] = useState("EUR");
+  const [currency, setCurrency] = useState("ADA");
   const [amount, setAmount] = useState("");
 
   const onFormSubmit = (event) => {
